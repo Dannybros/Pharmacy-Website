5335fe6e595f047d3f8cff1d0a1531f3f4788c46
Danny Bros <akbaromega89@gmail.com>
2022-03-26 19:25:44 +0700

    made responsive sign in page

--- client/src/components/User/SIgnIn.js
+++ client/src/components/User/SIgnIn.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react'
 import './Login.scss';
 import {Row, Col, Button} from 'react-bootstrap'
-import PersonIcon from '@mui/icons-material/Person';
-import LockIcon from '@mui/icons-material/Lock';
 
 function SignIn() {
 
@@ -15,60 +13,52 @@ function SignIn() {
 
   return (
     <div className='login_page'>
-      <form className='p-3' onSubmit={handleSubmit}>
-        <h1> {!signUpState ? "Login":"Sign Up"}</h1>
+      <form className='px-4 py-3' onSubmit={handleSubmit}>
+        <h3 className='mb-3'> {!signUpState ? "Login":"Sign Up"}</h3>
         <Row className='login_input_field'>
-          <Col md={12}>
-            <label><PersonIcon/> Username*</label>
-            <input type="text" className='form-control' name="username" placeholder='username...'/>
-          </Col>
-          <Col md={12}>
-            <label> <LockIcon/> Password*</label>
-            <input type="password" className='form-control' name='password' placeholder='password...'/>
-          </Col>
-          {signUpState &&
+          {signUpState ?
             <>
-              <Col md={6}>
-                <label><PersonIcon/> First Name*</label>
-                <input type="text" className='form-control' name='firstName' placeholder='First Name...'/>
+              <Col xs={6}>
+                <input type="text" name='firstName' placeholder='First Name...'/>
               </Col>
-              <Col md={6}>
-                <label><PersonIcon/> Second Name*</label>
-                <input type="text" className='form-control' name='secondName' placeholder='Second Name...'/>
+              <Col xs={6}>
+                <input type="text" name='secondName' placeholder='Second Name...'/>
               </Col>
-              <Col md={6}>
-                <label><PersonIcon/> Age*</label>
-                <input type="text" className='form-control' name='age' placeholder='Age...'/>
+              <Col xs={6}>
+                <input type="text" name='age' placeholder='Age...'/>
               </Col>
-              <Col md={6}>
-                <label><PersonIcon/> Contact Number*</label>
-                <input type="text" className='form-control' name='contact' placeholder='Contact Number...'/>
+              <Col xs={6}>
+                <input type="text" name='contact' placeholder='Contact Number...'/>
               </Col>
-              <Col md={12}>
-                <label><PersonIcon/> Address*</label>
-                <input type="text" className='form-control' name='address' placeholder='Address...'/>
+              <Col xs={12}>
+                <input type="text" name='address' placeholder='Address...'/>
               </Col>
-              <Col md={6}>
-                <label><PersonIcon/> UserName*</label>
-                <input type="text" className='form-control' name='contact' placeholder='UserName...'/>
+              <Col xs={6}>
+                <input type="text" name='contact' placeholder='Username...'/>
               </Col>
-              <Col md={6}>
-                <label><PersonIcon/> Password*</label>
-                <input type="password" className='form-control' name='password' placeholder='Password...'/>
+              <Col xs={6}>
+                <input type="password" name='password' placeholder='Password...'/>
               </Col>
-              <Col md={6}>
-                <label><PersonIcon/> Confirm Password*</label>
-                <input type="password" className='form-control' name='cpassword' placeholder='Confirm Password...'/>
+              <Col xs={12}>
+                <input type="password" name='cpassword' placeholder='Confirm Password...'/>
+              </Col>
+            </>:
+            <>
+              <Col xs={12}>
+                <input type="password" name="username" placeholder='Username...'/>
+              </Col>
+              <Col xs={12}>
+                <input type="password" name='password' placeholder="Password..."/>
               </Col>
             </>
           }
         </Row>
         
-        <div className='d-flex' style={{gap:"10px"}}>
-          <Button variant='primary' className='login_button mt-4 w-50' type="submit">
+        <div className='sign_btn_group'>
+          <Button variant='primary' className='login_button w-50' type="submit">
             {signUpState? "Sign Up" : "Log In"}
           </Button>
-          <Button variant='danger' className='login_button mt-4 w-50' type="reset">Cancel</Button>
+          <Button variant='danger' className='login_button w-50' type="reset">Cancel</Button>
         </div>
 
         {signUpState? 
