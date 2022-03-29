1e035aff9b9e90a1c4cc8a97d3f21f823a2b9ad4
Danny Bros <akbaromega89@gmail.com>
2022-03-29 20:34:30 +0700

    tested transform effect on scroll

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Home.scss'
 import { Container} from 'react-bootstrap';
 
 function Home() {
+
+  const [skew1, setSkew1] = useState(-15);
+  const [skew2, setSkew2] = useState(15);
+
+  window.addEventListener('scroll', ()=>{
+    setSkew1( -15 + window.scrollY/45);
+    setSkew2(15 + window.scrollY/-45);
+  })
+
   return (
     <Container>
-
+      <section className='tester'>
+        bg1
+        <span className='skew' style={{transform:`skewY(${skew1}deg)`}}></span>
+        <span className='skew_two' style={{transform:`skewY(${skew2}deg)`}}></span>
+      </section>
+      <section className='test2'>
+        bg2
+      </section>
     </Container>
   )
 }
