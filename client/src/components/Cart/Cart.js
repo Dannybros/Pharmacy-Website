6aa7034f53f4410c494b8d16288348c1dc5f15a9
Danny Bros <akbaromega89@gmail.com>
2022-04-14 21:10:13 +0700

    finished new cart list webpage

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -16,33 +16,51 @@ function Cart() {
                 <CloseIcon className='del_list_btn'/>
                 <img src={testItem} alt=""/>
                 <div className='cart_item_info'>
+                  <div className='cart_item_name'>
+                    <h3>Cart Item Title</h3>
+                    <Button>
+                      View
+                    </Button>
+                  </div>
 
                   <div className='cart_item_price'>
-                    <h3>Cart Item Title</h3>
+                    <h3>20$</h3>
                     <div className=' cart_item_counter'>
                       <PlayArrowIcon className='item_counter_icon' style={{transform:"rotate(180deg)"}}/> 
                       0 
                       <PlayArrowIcon className='item_counter_icon'/>
                     </div>
-                    <p>20$ * 2 = <span style={{color:"#0B5ED6"}}><b>305pc</b></span> </p>
                   </div>
+                </div>
+              </li>
 
-                  <div className='cart_item_btn_box'>
+              <li className='d-flex'>
+                <CloseIcon className='del_list_btn'/>
+                <img src={testItem} alt=""/>
+                <div className='cart_item_info'>
+                  <div className='cart_item_name'>
+                    <h3>Cart Item Title</h3>
                     <Button>
                       View
                     </Button>
-                    <Button variant='danger'>
-                      Delete
-                    </Button>
                   </div>
 
+                  <div className='cart_item_price'>
+                    <h3>20$</h3>
+                    <div className=' cart_item_counter'>
+                      <PlayArrowIcon className='item_counter_icon' style={{transform:"rotate(180deg)"}}/> 
+                      0 
+                      <PlayArrowIcon className='item_counter_icon'/>
+                    </div>
+                  </div>
                 </div>
               </li>
             </ul>
           </Col>
-          <Col md={4} sm={12}>
-            <div className='d-flex-column'>
 
+          <Col md={4} sm={12} className="p-3">
+            <div className='d-flex total_checkout_box'>
+              <h3>Total</h3>
             </div>
           </Col>
         </Row>
